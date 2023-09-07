cd6b9c3c61820c68c7e329b23b697ebd4fb2c7c8
kaokb <kaokabkareem@gmail.com>
2023-09-08 00:14:14 +0300

    steg

--- controllers/stegController.js
+++ controllers/stegController.js
@@ -25,11 +25,45 @@ exports.enStegImage = async (req, res) => {
     const { path: imagePath } = req.file;
 
     ////   karriiim
-    const outimg = '';
-    /////
+const binaryKey = sessionKey
+    .split('')
+    .map((char) => char.charCodeAt(0).toString(2).padStart(8, '0'))
+    .join('');
 
-    // Respond with the steganographic image file
-    res.sendFile(outimg);
+    ///////////////
+    const canvas = document.createElement('canvas');
+    canvas.width = imagePath.width;
+    canvas.height = imagePath.height;
+    const ctx = canvas.getContext('2d');
+    ctx.drawImage(imagePath, 0, 0);
+  
+    const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+    const pixels = imageData.data;
+  
+    // Convert secret message to binary
+    const binaryMessage = message
+      .split('')
+      .map((char) => char.charCodeAt(0).toString(2).padStart(8, '0'))
+      .join('');
+  
+    // Check if the image can hold the entire secret message
+    if (binaryMessage.length > pixels.length) {
+      throw new Error('The secret message is too large to be encoded in the image.');
+    }
+  
+    let binaryIndex = 0;
+    for (let i = 0; i < pixels.length; i += 4) {
+      if (binaryIndex < binaryMessage.length) {
+        // Modify the least significant bit of each color channel
+        pixels[i] = (pixels[i] & 0xFE) | (parseInt(binaryMessage[binaryIndex], 10) ^ parseInt(binaryKey[binaryIndex % binaryKey.length], 10));
+        binaryIndex++;
+      }
+    }
+    ctx.putImageData(imageData, 0, 0);
+    var img = new Image();
+    img.src = canvas.toDataURL();
+    document.body.appendChild(img);
+    res.sendFile(img);
   } catch (error) {
     console.error('Error encoding message:', error);
     res.status(500).send('Error encoding message.');
@@ -40,9 +74,43 @@ exports.enStegImage = async (req, res) => {
 exports.deStegImage = async (req, res) => {
   try {
     const { path: imagePath, sessionKey } = req.file;
+    ////////
+    const binaryKey = sessionKey
+    .split('')
+    .map((char) => char.charCodeAt(0).toString(2).padStart(8, '0'))
+    .join('');
 
+    //////////////////
+    const canvas = document.createElement('canvas');
+    canvas.width = imagePath.width;
+    canvas.height = imagePath.height;
+    const ctx = canvas.getContext('2d');
+    ctx.drawImage(imagePath, 0, 0);
+  
+    const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+    const pixels = imageData.data;
+  
+    let binaryMessage = '';
+    let binaryIndex = 0; // Add this line to initialize binaryIndex
+    for (let i = 0; i < pixels.length; i += 4) {
+      binaryMessage += (pixels[i] & 1) ^ parseInt(binaryKey[binaryIndex % binaryKey.length], 10);
+      binaryIndex++; // Increment binaryIndex after each iteration
+    }
+  
+    // Convert binary message to string
+    let secretMessage = '';
+    for (let i = 0; i < binaryMessage.length; i += 8) {
+      const byte = binaryMessage.substr(i, 8);
+      const charCode = parseInt(byte, 2);
+      if(charCode===33)
+      {
+        break;
+      }
+      secretMessage += String.fromCharCode(charCode);
+    }
+    ////////////
     // Respond with the decoded message
-    res.send({ message });
+    res.send({ secretMessage });
   } catch (error) {
     console.error('Error decoding message:', error);
     res.status(500).send('Error decoding message.');
