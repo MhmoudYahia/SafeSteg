f03d95bc3a2641219085b2c8792213e17f690996
John Doe <myehia162@gmail.com>
2023-10-04 23:45:43 +0300

    Done🫡

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -8,11 +8,11 @@ const userController = require('../controllers/userController');
 router.route('/signup').post(authController.signup);
 router.route('/signin').post(authController.signin);
 router.route('/signout').post(authController.signout);
+router.route('/forgetpassword').post(authController.forgotPassword);
+router.route('/resetpassword/:token').patch(authController.resetPassword);
 
 router.use(authController.protect);
 router.route('/me').get(userController.getMe);
-router.route('/forgetpassword').post(authController.forgotPassword);
-router.route('/resetpassword/:token').patch(authController.resetPassword);
 router.route('/changepassword').patch(authController.changePassword);
 router
   .route('/changeme')
